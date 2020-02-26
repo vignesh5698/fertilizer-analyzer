92ba5978d720ddc7070980ebc38cb58fab634e1e
vignesh <vigneshit13@gmail.com>
2020-02-26 23:42:35 +0530

    hot fix v2

--- src/components/MasterPage.js
+++ src/components/MasterPage.js
@@ -7,8 +7,8 @@ class MasterPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLogged: !false,
-      showRecommendation: !false,
+      isLogged: false,
+      showRecommendation: false,
       inputValues: null
     }
   }
